f6fa4e6af244c748bd6231572a0b8c7efa9782ea
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-07 09:39:53 -0600

    Add functions to utils

--- utils.js
+++ utils.js
@@ -6,3 +6,10 @@ export function getElement(selector, scope = document) {
     return scope.getElementById(selector);
 }
 
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
